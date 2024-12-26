41225e7426598891864b39154dc0db38c5499198
sazan10 <sazzzan10@gmail.com>
2024-12-27 02:16:16 +0545

    first commit

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -6,11 +6,19 @@ package com.mycompany.app;
 public class App {
 
     private static final String MESSAGE = "Hello World!";
+    private static int[] numbers = {1, 2, 3, 4, 5};
+    private static int sum = 0;
 
     public App() {}
 
     public static void main(String[] args) {
         System.out.println(MESSAGE);
+        for (int i = 0; i < numbers.length; i++) {
+            sum += numbers[i];
+            System.out.println("After processing element " + (i + 1) + ", sum = " + sum);
+        }
+
+        System.out.println("Total sum of array: " + sum);
     }
 
     public String getMessage() {
